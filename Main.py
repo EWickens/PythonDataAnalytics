8f1ef5234cd807533368d6548fdf91510dd96f2a
Eoin Wickens <eoin.wickens@mycit.ie>
2018-11-27 14:29:35 +0000

    Overall Pos/Neg value of the word is now derived from the previous two columns

--- Main.py
+++ Main.py
@@ -14,7 +14,13 @@ def main():
     posDataFrame = pd.DataFrame(list(posDict.items()), columns=['Word', 'Positive Freq'])
     negDataFrame = pd.DataFrame(list(negDict.items()), columns=['Word', 'Negative Freq'])
 
+    #Merges the two dataframes to get a joined list with both positive and negative words.
+
     result = pd.merge(posDataFrame, negDataFrame, on='Word', how='outer')
+
+    #Adds a third column to the dataframe which is the overall result of the word to determine if its classed as positive or negative.
+    result['Overall'] = result['Positive Freq'] - result['Negative Freq']
+
     print(result)
 
 

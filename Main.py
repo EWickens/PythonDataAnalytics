562f2e09e3e25d3de082142ccebf833487a6e9b1
Eoin Wickens <eoin.wickens@mycit.ie>
2018-12-04 14:21:30 +0000

    Committing before stripping out dataframes

--- Main.py
+++ Main.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 
 def main():
-    posUnique = getUniqueWordsFromList("testPos.txt")
-    negUnique = getUniqueWordsFromList("testNeg.txt")
+    posUnique = getUniqueWordsFromList("trainPos.txt")
+    negUnique = getUniqueWordsFromList("trainNeg.txt")
 
     # total_word_list = posUnique.union(negUnique)
 
-    posDict = getFreqOfWords("testPos.txt")
-    negDict = getFreqOfWords("testNeg.txt")
+    posDict = getFreqOfWords("trainPos.txt")
+    negDict = getFreqOfWords("trainNeg.txt")
 
     posDataFrame = pd.DataFrame(list(posDict.items()), columns=['Word', 'Positive Freq'])
     negDataFrame = pd.DataFrame(list(negDict.items()), columns=['Word', 'Negative Freq'])
@@ -18,19 +18,30 @@ def main():
 
     result = pd.merge(posDataFrame, negDataFrame, on='Word', how='outer')
 
-    #Adds a third column to the dataframe which is the overall result of the word to determine if its classed as positive or negative.
-    result['Overall'] = result['Positive Freq'] - result['Negative Freq']
+    #Replaces the nulls with 0
+    result = result.fillna(0)
 
+    #Adds a third column to the dataframe which is the overall result of the word to determine if its classed as positive or negative.
+    result['Overall'] = result['Positive Freq'] + result['Negative Freq']
+
+
+
+    result = calculateProbabilityOfWord(result)
     print(result)
 
 
-def loadDataFromFile(name):
+def loadTestDataFromFile(name):
     dataFile = open("dataFiles/test/" + name, "r")
     return dataFile;
 
+def loadTrainingDataFromFile(name):
+    dataFile = open("dataFiles/train/" + name, "r")
+    return dataFile;
+
+
 
 def getUniqueWordsFromList(name):
-    data = loadDataFromFile(name)
+    data = loadTrainingDataFromFile(name)
     vocab = set();
 
     dataRead = data.read()
@@ -41,14 +52,28 @@ def getUniqueWordsFromList(name):
 
     return vocab;
 
-
+# Returns a dictionary with the individual words + Frequency at which they occur within the dataset
 def getFreqOfWords(name):
-    data = loadDataFromFile(name)
+    data = loadTrainingDataFromFile(name)
     dataRead = data.read()
 
     dictionary = dict(Counter(dataRead.split()))
 
     return dictionary;
+
+def calculateProbabilityOfWord(result):
+    result['PosProb'] = result['Positive Freq'] / result['Overall']
+    result['NegProb'] = result['Negative Freq'] / result['Overall']
+
+    return result
+
+def detProbOfTweet(result):
+    negTestSet = loadTestDataFromFile("testNeg")
+    posTestSet = loadTestDataFromFile("testPos")
+
+    negLines = negTestSet.readlines()
+    posLines = posTestSet.readLines()
+
 
 
 main()
